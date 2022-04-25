6a8344ed9fef166cf0b72dc11ffcd063e333c149
Konrad Gräfe <kgraefe@paktolos.net>
2022-04-25 13:43:57 +0200

    [refactor] Use MimeParser to parse raw message
    Signed-off-by: Konrad Gräfe <kgraefe@paktolos.net>

--- background.js
+++ background.js
@@ -1,46 +1,5 @@
 messenger.messageDisplay.onMessageDisplayed.addListener(async(tab, message) => {
-	messenger.messages.getRaw(message.id).then((binaryString) => {
-		attachments = {};
-
-		isPGPUniversal = false;
-		plaintextName = null;
-
-		binaryString.split(/(?<!;)\r\n(?!\s)/g).every((line) => {
-			if (line.startsWith("X-PGP-Universal: processed")) {
-				isPGPUniversal = true;
-			}
-			if (!isPGPUniversal) {
-				if (line == "") {
-					/* We processed all mail headers and did not find a trace
-					 * of PGP Universal. Stop here.
-					 */
-					return false;
-				}
-				return true;
-			}
-
-			if (line.startsWith("X-Content-PGP-Universal-Saved-Content-Description: ")) {
-				plaintextName = line
-					.substring( line.indexOf(" ") + 1 )
-					.replace(/\r\n\s*/g, "");
-			}
-			if (line.startsWith("X-Content-PGP-Universal-Saved-Content-Type: text/calendar")) {
-				plaintextName = "Event.ics"
-			}
-			if (plaintextName == null) {
-				return true;
-			}
-			if (line.startsWith("Content-Type: ")) {
-				fileName = line.split("\"")[1];
-				attachments[fileName] = plaintextName + ".pgp";
-				plaintextName = null;
-			}
-			return true;
-		});
-
-		if (isPGPUniversal) {
-			console.log(attachments);
-			messenger.renameattachments.setReplacements(attachments);
-		}
+	messenger.messages.getRaw(message.id).then((rawmsg) => {
+		messenger.pgpuniversal.parseMessage(rawmsg);
 	});
 });
---
+++ pgpuniversal.js
@@ -0,0 +1,70 @@
+const { AddonManager } = ChromeUtils.import(
+	"resource://gre/modules/AddonManager.jsm"
+);
+const EnigmailMsgRead = ChromeUtils.import(
+  "chrome://openpgp/content/modules/msgRead.jsm"
+).EnigmailMsgRead;
+
+var { MimeParser } = ChromeUtils.import("resource:///modules/mimeParser.jsm");
+
+// Global variables
+var Replacements = {};
+
+// Install hook into EnigmailMsgRead.getAttachmentName
+var EnigmailGetAttachmentName = EnigmailMsgRead.getAttachmentName;
+EnigmailMsgRead.getAttachmentName = function(attachment) {
+	if (attachment.name in Replacements) {
+		return Replacements[ attachment.name ];
+	} else {
+		return EnigmailGetAttachmentName(attachment);
+	}
+};
+
+// Uninstall hook when addon gets disabled
+AddonManager.addAddonListener({
+	onDisabling(addon, needsRestart) {
+		EnigmailMsgRead.getAttachmentName = EnigmailGetAttachmentName;
+	}
+});
+
+// Implement API
+var pgpuniversal = class extends ExtensionCommon.ExtensionAPI {
+	getAPI(context) {
+		return {
+			pgpuniversal: {
+				parseMessage(rawmsg) {
+					// Parse message
+					var msg = MimeParser.extractMimeMsg(rawmsg, {});
+
+					// Check if we got a PGP Universal mail
+					if (msg === undefined || !("x-pgp-universal" in msg.headers)) {
+						return;
+					}
+
+					// Find plaintext attachment names
+					Replacements = {}
+					for (let attachment of msg.allAttachments) {
+						let description = attachment.headers["x-content-pgp-universal-saved-content-description"];
+						if (description !== undefined && description.length > 0) {
+							Replacements[attachment.name] = description[0];
+						} else {
+							let type = attachment.headers["x-content-pgp-universal-saved-content-type"];
+							if (type !== undefined && type.length > 0 && type[0].startsWith("text/calendar;")) {
+								Replacements[attachment.name] = "Event.ics";
+							}
+						}
+					}
+
+					// Update displayed file names in attachment bar
+					for (let win of Services.wm.getEnumerator("mail:3pane", true)) {
+						for (let lbl of win.document.getElementsByClassName("attachmentcell-name")) {
+							if (lbl.value in Replacements) {
+								lbl.value = Replacements[lbl.value];
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+};
